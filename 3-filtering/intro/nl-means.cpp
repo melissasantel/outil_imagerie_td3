2b2906433a0d7a61a97631a80cb9e97131fe8737
SANTEL Melissa <msantel@jolinum.pedago.ipb.fr>
2019-10-23 22:35:59 +0200

    Exercice nl-means

--- 3-filtering/intro/nl-means.cpp
+++ 3-filtering/intro/nl-means.cpp
@@ -7,10 +7,14 @@
 using namespace cv;
 using namespace std;
 
+double
+computeTreshDistance(int sigma){
+
+}
+
 void
 process(int sigma, const char* imsname, const char* imdname)
 {
-  (void) sigma;
   (void) imdname;
   //Check the existence of the file, if the file doesn't exist the programme stop
   fstream infile(imsname);
@@ -25,19 +29,42 @@ process(int sigma, const char* imsname, const char* imdname)
 
   Mat imd(s,CV_8UC1);
   //According to author the patch tall should be 7*7. We can deduce r=3
-  int radius = 3;
-  int
-  for(int i=0; i<s.height; i++){
-    for(int j=0; j<s.width; j++){
-      for(int l=-radius; l<=radius; l++){
-        for(int m=-radius; m<=radius; m++){
-          if(i+l>=0 && j+m>=0 && i+l<s.height && j+m<s.width){
-
-          }
-        }
-      }
-    }
-  }
+  int patch_radius = 3;
+  //According to author the window tall should be 11*11. We can deduce r=5
+  int window_radius = 5;
+  int patchs_sum;
+  int pi,pj,qi,qj=0;
+  int w;
+  int sum_w;
+  int sum_w_means;
+  // for(int i=0; i<s.height; i++){
+  //   for(int j=0; j<s.width; j++){
+  //     patchs_sum =0;
+  //     pi=i;
+  //     pj=j;
+  //     w=0;
+  //     sum_w=0;
+  //     for(int l=-window_radius; l<=window_radius; l++){
+  //       for(int m=-window_radius; m<=window_radius; m++){
+  //         if(i+l>=0 && j+m>=0 && i+l<s.height && j+m<s.width){
+  //           qi=i+l;
+  //           qj=j+m;
+  //           for(int u=-patch_radius; u<=patch_radius; u++){
+  //             for(int v=-patch_radius; v<=patch_radius; u++){
+  //               if(i+u>=0 && j+v>=0 && i+u<s.height && j+v<s.width && qi+u>=0 && qj+v>=0 && qi+u<s.height && qj+v<s.width){
+  //                 patchs_sum +=pow(ims.at<uchar>(pi+u,pj+v)-ims.at<uchar>(qi+u,qj+v),2);
+  //               }
+  //             }
+  //           }
+  //           w = exp((-1)*(patchs_sum/pow(2*patch_radius+1,1))/2*pow(sigma,2));
+  //           sum_w += w;
+  //           sum_w_means += w*ims.at<uchar>(qi,qj);
+  //         }
+  //       }
+  //     }
+  //     cout << sum_w << endl;
+  //   }
+  // }
 
 }
 
